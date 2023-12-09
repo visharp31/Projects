e0fba8c29b4fda9ad4b03d48043a2997529420b5
Vishal Singh <singh287686@gmail.com>
2023-12-09 15:51:25 +0530

    added code for diff.java

--- Diff.java
+++ Diff.java
@@ -1,3 +1,13 @@
+import java.util.Scanner;
+
 public class Diff {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter two numbers :");
+        int a = sc.nextInt();
+        int b =sc.nextInt();
+        int dif = a-b;
+        System.out.println("The difference of 2 numbers is : "+dif);
+        sc.close();
+    }
 }
